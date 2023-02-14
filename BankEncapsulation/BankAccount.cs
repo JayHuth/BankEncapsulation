867296e13a9c3a9be5bda6d1f4255d81aa3f2e6f
Justin Huth <Jayhuth96@gmail.com>
2023-02-14 14:55:00 -0600

    Completed the excercise

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Accepting a deposit of {amount, 0:c} to your account.");
+            _balance = amount;
+        }
+
+        //public void Withdraw(double amount)
+        //{
+        //    Console.WriteLine($"Initiating your withdraw of {amount,0:c} from your account.");
+        //    _balance = amount;
+        //}
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+    }
+
+        
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount ba = new BankAccount();
+
+            Console.WriteLine("How much money will you be depositing today?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(amountToDeposit);
+            double userBalance = ba.GetBalance();
+
+            Console.WriteLine($"your current balance is: {userBalance, 0:c}");
+        }
+
+        static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount();
+
+            Console.WriteLine("How much money will you be withdrawing today?");
+            double amountToWithdraw = double.Parse(Console.ReadLine());
+
+            ba.Withdraw(amountToWithdraw);
+            double userBalance = ba.GetBalance();
+
+            Console.WriteLine($"your balance is: {userBalance,0:c}");
         }
     }
 }
